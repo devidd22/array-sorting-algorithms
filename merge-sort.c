710ef8e085145d865bb7dbfaf3d124cf46acac27
devidd22 <mihail_devid.mecu@stud.acs.upb.ro>
2023-11-27 15:05:53 +0200

    Add Merge Sort algorithm skeleton

---
+++ merge-sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+static void sort(void)
+{
+	if (begin >= end)
+        return;
+ 
+    int mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+
+int main()
+{
+	return 0;
+}
